0abd438809233f29960a664fdaab54887db1d258
hammm638 <ilhamkholif123123@gmail.com>
2026-05-18 15:24:40 +0700

    Update MainActivity.java

--- app/src/main/java/com/webview/MainActivity.java
+++ app/src/main/java/com/webview/MainActivity.java
@@ -1,1 +1,22 @@
+package com.webview;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        WebSettings ws = webView.getSettings();
+        ws.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://example.com");
+        setContentView(webView);
+    }
+}
